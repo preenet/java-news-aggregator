540049f08fd321ed74670dc770c062672317c1ae
preenet <preenet@e99d27a7-f373-458f-9eab-32f64f2e7925>
2011-08-20 07:59:37 +0000

    Initial import.

---
+++ CJNA/src/CJNA.java
@@ -0,0 +1,10 @@
+/**
+ * @author Pree Thiengburanathum
+ * @version 0.1
+ */
+
+public class CJNA {
+	public static void main(String args[]) {
+		System.out.println("Hello world!");
+	}
+}// end class CJNA
