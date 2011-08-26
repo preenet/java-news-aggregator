f2c1865f5382e3caef0f63712fe49616a6861c1a
preenet <preenet@e99d27a7-f373-458f-9eab-32f64f2e7925>
2011-08-26 16:19:27 +0000

    updated: uml model

--- CJNA/src/connection/HTTPConnectionSelection.java
+++ CJNA/src/connection/HTTPConnectionSelection.java
@@ -12,30 +12,45 @@ import org.apache.commons.httpclient.HttpException;
 public class HTTPConnectionSelection {
 	
 	private String URI;
- 	private URL url;
     private URLConnection urlConn; 
     private BufferedReader reader;
     
 	public HTTPConnectionSelection(String URI) {
 		this.URI = URI;
 	}
-	
+	/**
+	 * Method calling to use the direct connection.
+	 * @throws IOException
+	 */
 	public void DirectConnect() throws IOException {
 		System.out.println("Connecting without proxy...");
 		HTTPDirectConnection dc = new HTTPDirectConnection(this.URI);
 		this.urlConn = dc.getURLConnection();
 	}
 	
+	/**
+	 * Method calling to use the proxy connection.
+	 * @throws HttpException
+	 * @throws IOException
+	 */
 	public void ProxyConnect() throws HttpException, IOException {
 		System.out.println("Connecting with proxy...");
 		HTTPProxyConnection proxy = new HTTPProxyConnection(this.URI);
 		this.reader = proxy.getBufferedReader();
 	}
-
+	
+	/**
+	 * Return method returning url connection.
+	 * @return urlConn
+	 */
 	public URLConnection getURLConnection() {
 		return this.urlConn;
 	}
 	
+	/**
+	 * Return method return the buffered.
+	 * @return reader
+	 */
 	public BufferedReader getBufferedReader() {
 		return this.reader;
 	}
