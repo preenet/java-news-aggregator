885c0670edb166e23b68ec4a6a75e4e541269716
preenet <preenet@e99d27a7-f373-458f-9eab-32f64f2e7925>
2011-08-29 08:51:22 +0000

    added: prox user interface action listerner. when check box is checked user can enter inputs in text fields. For default setting , user can't enter anything.

--- CJNA/src/cjna/ui/ProxyUI.java
+++ CJNA/src/cjna/ui/ProxyUI.java
@@ -2,27 +2,31 @@ package cjna.ui;
 
 import javax.swing.*;
 import java.awt.*;
-
-public class ProxyUI extends JFrame {
-
- /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 /**
  * @author Pree 
  */
+public class ProxyUI extends JFrame {
+private static final long serialVersionUID = 1L;
+private boolean isProxy = false;
+private JTextField AddressTextField = new JTextField();
+private JTextField PortTextField = new JTextField();
+private JTextField DomainNameTextField = new JTextField();
+private JTextField UserNameTextField = new JTextField();
+private JTextField PassWordTextField = new JTextField();
+
 public ProxyUI( ) {
 	super("Proxy Setting");
 	
-    JTextField AddressTextField = new JTextField();
-    JTextField PortTextField = new JTextField();
-    JTextField DomainNameTextField = new JTextField();
-    JTextField UserNameTextField = new JTextField();
-    JTextField PassWordTextField = new JTextField();
-       
-       
+    // set unable to text fields as default setting.
+    AddressTextField.setEnabled(false);
+	PortTextField.setEnabled(false);
+	DomainNameTextField.setEnabled(false);
+	UserNameTextField.setEnabled(false);
+	PassWordTextField.setEnabled(false);
+	
+	// set Frame properties and layout
   
     setSize(300,180);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -31,33 +35,71 @@ public ProxyUI( ) {
     c1.add(new JLabel(" Proxy Server "));
     
     JCheckBox proxyCheckBox = new JCheckBox();
-    proxyCheckBox.setSelected(true);
+    proxyCheckBox.setSelected(false);
+    
+    proxyCheckBox.addItemListener(
+    	    new ItemListener() {
+    	        public void itemStateChanged(ItemEvent e) {
+    	        	isProxy = (e.getStateChange() == ItemEvent.SELECTED);
+    	        	 if(!isProxy) {
+    	        		 AddressTextField.setEnabled(false);
+    	        		 PortTextField.setEnabled(false);
+    	        		 DomainNameTextField.setEnabled(false);
+    	        		 UserNameTextField.setEnabled(false);
+    	        		 PassWordTextField.setEnabled(false);
+    	        	 }
+    	        	 else {
+    	        		 AddressTextField.setEnabled(true);
+    	        		 PortTextField.setEnabled(true);
+    	        		 DomainNameTextField.setEnabled(true);
+    	        		 UserNameTextField.setEnabled(true);
+    	        		 PassWordTextField.setEnabled(true);
+    	        	 }
+    	        }
+    	    }
+    	);
+    
     c1.add(proxyCheckBox);
     
-    
-    
-    // container 2 for proxy setting
-    Container c2 = getContentPane();
-    c2.setLayout(new GridLayout(0,2));
-
-
+    c1.setLayout(new GridLayout(0,2));
    
     // add labels and text fields to the container
-    c2.add(new JLabel(" Address "));
-    c2.add(AddressTextField);
-    c2.add(new JLabel(" Port "));
-    c2.add(PortTextField);
-    c2.add(new JLabel(" Domain Name "));
-    c2.add(DomainNameTextField);
-    c2.add(new JLabel(" User Name"));
-    c2.add(UserNameTextField);
-    c2.add(new JLabel(" Password "));
-    c2.add(PassWordTextField);
+    c1.add(new JLabel(" Address "));
+    c1.add(AddressTextField);
+    c1.add(new JLabel(" Port "));
+    c1.add(PortTextField);
+    c1.add(new JLabel(" Domain Name "));
+    c1.add(DomainNameTextField);
+    c1.add(new JLabel(" User Name"));
+    c1.add(UserNameTextField);
+    c1.add(new JLabel(" Password "));
+    c1.add(PassWordTextField);
 
     setVisible(true);
-  }
+  }// end constructor
 
-  public static void main(String args[]) {
-    new ProxyUI( );
-  }
+	// setter and getter here.
+	public String getAddress() {
+		return this.AddressTextField.getText();
+	}
+	
+	public String getPort() {
+		return this.PortTextField.getText();
+	}
+	
+	public String getDomain() {
+		return this.DomainNameTextField.getText();
+	}
+	
+	public String getUserNameTextField () {
+		return this.UserNameTextField.getText();
+	}
+	
+	public String PassWordTextField() {
+		return this.PassWordTextField.getText();
+	}
+	
+	public static void main(String args[]) {
+		new ProxyUI();
+	}
 }// end class ProxyUI
