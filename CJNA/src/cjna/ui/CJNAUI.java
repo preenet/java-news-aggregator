383252904408bf6be7f6bd000c4bef8597b39b4a
preenet <preenet@e99d27a7-f373-458f-9eab-32f64f2e7925>
2011-09-01 06:50:57 +0000

    added CJNAUI design

---
+++ CJNA/src/cjna/ui/CJNAUI.java
@@ -0,0 +1,60 @@
+package cjna.ui;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import java.awt.Color;
+
+
+public class CJNAUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CJNAUI frame = new CJNAUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CJNAUI() {
+		setTitle("CJNA");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(6, 248, 438, 24);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel consoleLabel = new JLabel("");
+		consoleLabel.setBounds(6, 6, 426, 16);
+		panel_1.add(consoleLabel);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBackground(Color.WHITE);
+		textArea.setBounds(435, 6, -424, 235);
+		contentPane.add(textArea);
+	}
+}
