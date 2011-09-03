6b4bc18a82dee482dad832677edc089a2a13411d
preenet <preenet@e99d27a7-f373-458f-9eab-32f64f2e7925>
2011-09-03 08:13:21 +0000

    ACCEPTED - task 15: Removing duplicate element in the global list when fetching more news http://pree.unfuddle.com/projects/30371/tickets/by_number/15

---
+++ CJNA/src/org/cjna/parser/RemoveDuplicate.java
@@ -0,0 +1,22 @@
+package org.cjna.parser;
+
+import java.util.ArrayList;
+
+/**
+ * @author Pree
+ *
+ */
+public class RemoveDuplicate {
+	
+	/**
+	 * @param fmList
+	 * @param fm
+	 */
+	public RemoveDuplicate(ArrayList<FeedMessage> fmList, FeedMessage fm) {
+		// iterate through the array list and check from the title if the string is match the coming fm.
+		// if match, we then remove from the array list.
+		
+		
+	}
+
+}// end class RemoveDuplicate
