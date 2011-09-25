aad77ffa44d096b6ee3fd7969c0761896e418e51
preenet <preenet@e99d27a7-f373-458f-9eab-32f64f2e7925>
2011-09-25 02:50:30 +0000

    released

--- CJNALogFilter/src/CJNALogFilter.java
+++ CJNALogFilter/src/CJNALogFilter.java
@@ -1,29 +1,38 @@
 import java.io.*;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public final class CJNALogFilter implements Filter 
-{
-  private FilterConfig filterConfigObj = null;
+public final class CJNALogFilter implements Filter {
+	private FilterConfig filterConfigObj = null;
 
-  public void init(FilterConfig filterConfigObj) {
-  this.filterConfigObj = filterConfigObj;
-  }
+	public void init(FilterConfig filterConfigObj) {
+		this.filterConfigObj = filterConfigObj;
+	}
 
-  public void doFilter(ServletRequest request, 
-ServletResponse response,
-  FilterChain chain)
-  throws IOException, ServletException 
-  {
-  String remoteAddress =  request.getRemoteAddr();
-  String uri = ((HttpServletRequest) request).getRequestURI();
-  String protocol = request.getProtocol();
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		String remoteAddress = request.getRemoteAddr();
+		String uri = ((HttpServletRequest) request).getRequestURI();
+		String protocol = request.getProtocol();
+		String userName ="";
+		 if (request.getParameter("name") != null) {
+			  userName = request.getParameter("name");
+		 }
 
-  chain.doFilter(request, response);
-  filterConfigObj.getServletContext().log("Logging Filter Servlet called");
-    filterConfigObj.getServletContext().log("**************************");
-  filterConfigObj.getServletContext().log("User Logged ! " + " User IP: " + remoteAddress + " Resource File: " + uri + " Protocol: " + protocol);
-  }
+		chain.doFilter(request, response);
+		filterConfigObj.getServletContext()
+				.log("Logging Filter Servlet called");
+		filterConfigObj.getServletContext().log("**************************");
+		filterConfigObj.getServletContext().log(
+				"User Logged ! " + userName  + " User IP: " + remoteAddress
+						+ " Resource File: " + uri + " Protocol: " + protocol);
+	}
 
-  public void destroy() { }
-}
+}// end class CJNALofFilter
